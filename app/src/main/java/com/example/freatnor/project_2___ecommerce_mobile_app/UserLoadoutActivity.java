615320c4d38d2f0071370ab4204e2597a26cffa6
Freatnor <jonathan.randall.taylor@gmail.com>
2016-07-26 10:35:36 -0400

    working on fragment layouts

--- app/src/main/java/com/example/freatnor/project_2___ecommerce_mobile_app/UserLoadoutActivity.java
+++ app/src/main/java/com/example/freatnor/project_2___ecommerce_mobile_app/UserLoadoutActivity.java
@@ -9,23 +9,15 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SearchResultsActivity extends AppCompatActivity {
+public class UserLoadoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_results);
+        setContentView(R.layout.activity_user_loadout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
